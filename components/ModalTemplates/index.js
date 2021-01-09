46f23b3e2f2cb9dcc8d3d6dd5488944919a97fc7
umutkupa <71068005+umutkupa@users.noreply.github.com>
2021-01-09 15:35:42 +0300

    #4428 Add VIN field to Trade In step

--- components/ModalTemplates/index.js
+++ components/ModalTemplates/index.js
@@ -2,10 +2,12 @@ import OfferDetails from './OfferDetails.modal';
 import ProductVideos from './ProductVideos.modal';
 import Success from './Success.modal';
 import SuccessWide from './SuccessWide.modal';
+import Error from './Error.modal';
 
 export default {
   OfferDetails,
   Success,
+  Error,
   SuccessWide,
   ProductVideos
 };
